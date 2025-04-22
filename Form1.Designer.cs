0d88456fa92449c9181df6a6d8229d84a158898f
FrankieCook <nfcook097@gmail.com>
2025-04-22 00:26:30 -0500

    Playing with Elements

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,14 @@
     private void InitializeComponent()
     {
       this.button1 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
       this.SuspendLayout();
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(430, 198);
+      this.button1.Location = new System.Drawing.Point(377, 254);
       this.button1.Name = "button1";
       this.button1.Size = new System.Drawing.Size(75, 23);
       this.button1.TabIndex = 0;
@@ -41,21 +44,63 @@
       this.button1.UseVisualStyleBackColor = true;
       this.button1.Click += new System.EventHandler(this.button1_Click);
       // 
+      // label1
+      // 
+      this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+      this.label1.AutoSize = true;
+      this.label1.BackColor = System.Drawing.SystemColors.GrayText;
+      this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(-4, -2);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(511, 39);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Social Media Post Synchronization";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.label1.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.Location = new System.Drawing.Point(0, 37);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(168, 31);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "Social Media";
+      // 
+      // checkedListBox1
+      // 
+      this.checkedListBox1.FormattingEnabled = true;
+      this.checkedListBox1.Location = new System.Drawing.Point(-4, 71);
+      this.checkedListBox1.Name = "checkedListBox1";
+      this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+      this.checkedListBox1.TabIndex = 3;
+      this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.checkedListBox1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
       this.Controls.Add(this.button1);
       this.Name = "Form1";
       this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
 
     private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.CheckedListBox checkedListBox1;
   }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,12 +12,40 @@ namespace Social_Media_Post_Sync
 {
   public partial class Form1 : Form
   {
+    Label myControl;
+
     public Form1()
     {
       InitializeComponent();
+
+      myControl = new Label();
     }
 
     private void button1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void CenterControl(Control ctrl)
+    {
+      int x = (this.ClientSize.Width - ctrl.Width) / 2;
+      //int y = (this.ClientSize.Height - ctrl.Height) / 2;
+      int y = ctrl.Height;
+      ctrl.Location = new Point(x, y);
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      CenterControl(label1);
+      CenterControl(label2);
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
     {
 
     }
